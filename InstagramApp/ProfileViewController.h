b78ff022056f38ea1c2aa44ff97d9fd2b026de46
loneill <loneill@pool-ipv6-pd.agg2.rcl.cld-dbn.eircom.net>
2018-07-09 23:59:03 +0000

    Basic layout. 2 ViewControllers. ProfileViewcontroller layout done.

---
+++ InstagramApp/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  InstagramApp
+//
+//  Created by loneill on 09/07/2018.
+//  Copyright © 2018 loneill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end

6b338550411cadb4eb0b6bd14cc0bb790720656c
loneill <loneill@pool-ipv6-pd.agg2.rcl.cld-dbn.eircom.net>
2018-07-11 23:13:10 +0000

    Login button, text fields and segues

--- InstagramApp/ProfileViewController.h
+++ InstagramApp/ProfileViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ProfileViewController : UIViewController
+@interface ProfileViewController : UIViewController<NSURLConnectionDelegate>
+{
+    NSMutableData *_responseData;
+}
 
 @end
--- InstagramApp/ProfileViewController.m
+++ InstagramApp/ProfileViewController.m
@@ -8,7 +8,7 @@
 
 #import "ProfileViewController.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () 
 
 @end
 
@@ -17,7 +17,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    NSString *post = [NSString stringWithFormat:@"Username=%@&Password=%@",@"username",@"password"];
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"http://www.abcde.com/xyz/login.aspx"]];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:postData];
+    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+   // It returns the initialized url connection and begins to load the data for the url request.
+    //You can check that whether you URL connection is done properly or not using just if/else statement as below.
+        
+        if(conn) {
+            NSLog(@"Connection Successful");
+        } else {
+            NSLog(@"Connection could not be made");
+        }
+
+
 }
+// This method is used to receive the data which we get using post method.
+//- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData*)data
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+ _responseData = [[NSMutableData alloc] init];                                                //LON re -> _re
+}
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [_responseData appendData:data];
+}
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    
+    _responseData = nil;
+    NSLog(@"There was an error in connection");
+}
+-(void) connectionDidFinishLoading:(NSConnection *)connection{
+    
+    // NSLog(@"DONE. Received Bytes: %d", [responseData length]);
+    
+    NSString *theXML = [[NSString alloc] initWithBytes:
+                        [_responseData mutableBytes] length:[_responseData length] encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", theXML);
+}
+// This method receives the error report in case of connection is not made to server.
+//- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
+
+// This method is used to process the data after connection has made successfully.
+//- (void)connectionDidFinishLoading:(NSURLConnection *)connection
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
